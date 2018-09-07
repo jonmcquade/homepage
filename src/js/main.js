ca13eabb701b1ef5501e36f40cf4f54f381d815d
root <root@phin.jonmcquade.com>
2018-09-07 16:58:56 -0700

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+$("#design1").on("click", function() {
+    $('#imagepreview').attr('src', $('#design1').attr('src')); // here asign the image to the modal when the user click the enlarge link
+});
+
+$("#design2").on("click", function() {
+    $('#imagepreview').attr('src', $('#design2').attr('src')); // here asign the image to the modal when the user click the enlarge link
+});
+
+$("#shockedapplaud").on("click", function() {
+    $('#imagepreview').attr('src', $('#shockedapplaud').attr('src')); // here asign the image to the modal when the user click the enlarge link
+});
+
+$("#evilkitteh").on("click", function() {
+    $('#imagepreview').attr('src', $('#evilkitteh').attr('src')); // here asign the image to the modal when the user click the enlarge link
+});
+
+$("#bugle").on("click", function() {
+    $('#imagepreview').attr('src', $('#bugle').attr('src')); // here asign the image to the modal when the user click the enlarge link
+});
