74b20c5a26807533fdd0f5609f138a6064e4d0e6
root <root@phin.jonmcquade.com>
2018-09-20 19:45:42 -0700

    Adding scrollto positioning for mobile 'jumpt to' section

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
+
   $('#design1').on('click', function() {
     $('#imagepreview').attr('src', $('#design1').attr('src')); // here asign the image to the modal when the user click the enlarge link
   });
@@ -20,20 +21,20 @@ $( document ).ready(function() {
   });
   
   // Scrollto events for header/nav
-  $('#home').unbind().click(function() {
-    $.scrollTo($('a[name=top]').offset().top - 50,{duration: 500});
+  $('[id=home]').unbind().click(function() {
+    $.scrollTo($('a[name=top]').offset().top - 60,{duration: 500});
   });
 
-  $('#projects').unbind().click(function() {
-    $.scrollTo($('a[name=projects]').offset().top - 50,{duration: 500});
+  $('[id=projects]').unbind().click(function() {
+    $.scrollTo($('a[name=projects]').offset().top - 60,{duration: 500});
   });
 
-  $('#history').unbind().click(function() {
-    $.scrollTo($('a[name=history]').offset().top - 50,{duration: 500});
+  $('[id=history]').unbind().click(function() {
+    $.scrollTo($('a[name=history]').offset().top - 60,{duration: 500});
   });
 
-  $('#articles').unbind().click(function() {
-    $.scrollTo($('a[name=articles]').offset().top - 50,{duration: 500});
+  $('[id=articles]').unbind().click(function() {
+    $.scrollTo($('a[name=articles]').offset().top - 60,{duration: 500});
   });
 
 });
